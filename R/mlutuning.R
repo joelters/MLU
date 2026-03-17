e65a9c193aaaa0d829acaabacb53cd120ad1f769
joelters <joelrt2310@gmail.com>
2026-03-17 11:51:14 +0100

    fix tuning parameters grids

--- R/mlutuning.R
+++ R/mlutuning.R
@@ -12,7 +12,7 @@
 #' @param rf.cf.ntree.grid How many trees should be grown when using RF or CIF.
 #' @param rf.depth.grid How deep should trees be grown in RF. Inf means full depth (NULL in ranger).
 #' @param mtry.grid How many variables to consider at each split in RF. Defaults floor(sqrt(ncol(X))) and floor(ncol(X)/3) are always tried.
-#' @param cf.depth How deep should trees be grown in CIF (Inf is full depth, default from partykit).
+#' @param cf.depth.grid How deep should trees be grown in CIF (Inf is full depth, default from partykit).
 #' @param ensemblefolds.grid Integer specifying how many folds to use in ensemble methods such as OLSensemble or SuperLearner.
 #' @param polynomial.Lasso.grid Degree of polynomial to be fitted when using Lasso.
 #' @param polynomial.Ridge.grid Degree of polynomial to be fitted when using Ridge.
@@ -21,15 +21,9 @@
 #' @param polynomial.NLLS_exp.grid Degree of polynomial to be fitted when using NLLS_exp.
 #' @param polynomial.loglin.grid Degree of polynomial to be fitted when using loglin.
 #' @param xgb.nrounds.grid Number of boosting iterations for XGBoost.
-#' @param xgb.eta.grid Learning rate for XGBoost.
-#' @param xgb.depth.grid Maximum depth of trees for XGBoost.
-#' @param xgb.child_weight.grid Minimum sum of instance weight needed in a child for XGBoost.
-#' @param xgb.subsample.grid Subsample ratio of the training instances for XGBoost.
-#' @param xgb.colsample.grid Subsample ratio of columns when constructing each tree for XGBoost.
+#' @param xgb.max.depth.grid Maximum depth of trees for XGBoost.
 #' @param cb.iterations.grid Number of boosting iterations for CatBoost.
 #' @param cb.depth.grid Depth of the trees for CatBoost.
-#' @param cb.learning_rate.grid Learning rate for CatBoost.
-#' @param cb.l2_leaf_reg.grid L2 regularization coefficient for CatBoost.
 #' @param torch.hidden_units.grid List of hidden layer configurations for neural networks.
 #' @param torch.learning_rate.grid Learning rates for neural network training.
 #' @param torch.dropout.grid Dropout rates for neural networks.
@@ -58,15 +52,9 @@ mlutuning <- function(X, Y, f,
                        polynomial.NLLS_exp.grid = c(1, 2, 3),
                        polynomial.loglin.grid = c(1, 2, 3),
                        xgb.nrounds.grid = c(100, 200, 500),
-                       xgb.eta.grid = c(0.01, 0.1, 0.3),
-                       xgb.depth.grid = c(1, 3, 6),
-                       xgb.child_weight.grid = c(1, 5, 10),
-                       xgb.subsample.grid = c(0.5, 0.75, 1),
-                       xgb.colsample.grid = c(0.5, 0.75, 1),
+                       xgb.max.depth.grid = c(1, 3, 6),
                        cb.iterations.grid = c(100, 500, 1000),
                        cb.depth.grid = c(1, 3, 6, 10),
-                       cb.learning_rate.grid = c(0.01, 0.1, 0.3),
-                       cb.l2_leaf_reg.grid = c(1, 3, 5, 7, 9),
                        torch.hidden_units.grid = list(c(64, 32), c(128, 64), c(256, 128, 64)),
                        torch.learning_rate.grid = c(0.001, 0.01, 0.1),
                        torch.dropout.grid = c(0, 0.2, 0.5),
@@ -247,7 +235,7 @@ mlutuning <- function(X, Y, f,
         n_combinations <- length(rf.cf.ntree.grid) * length(rf.depth.grid) * length(mtry.grid)
         cat(sprintf("  - RF: testing %d combinations (trees × depth × mtry)\n", n_combinations))
       } else if (ml == "XGB") {
-        n_combinations <- length(xgb.nrounds.grid) * length(xgb.depth.grid)
+        n_combinations <- length(xgb.nrounds.grid) * length(xgb.max.depth.grid)
         cat(sprintf("  - XGB: testing %d combinations (rounds × depth)\n", n_combinations))
       } else if (ml == "CB") {
         n_combinations <- length(cb.iterations.grid) * length(cb.depth.grid)
@@ -275,7 +263,7 @@ mlutuning <- function(X, Y, f,
                polynomial.NLLS_exp.grid = polynomial.NLLS_exp.grid,
                polynomial.loglin.grid = polynomial.loglin.grid,
                xgb.nrounds.grid = xgb.nrounds.grid,
-               xgb.max.depth.grid = xgb.depth.grid,
+               xgb.max.depth.grid = xgb.max.depth.grid,
                cb.iterations.grid = cb.iterations.grid,
                cb.depth.grid = cb.depth.grid,
                torch.hidden_units.grid = torch.hidden_units.grid,
